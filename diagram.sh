3288940d614a208475a68ce7e86138ae807e11e3
K. Joeri van der Velde <joeriv@gmail.com>
2012-12-16 00:22:13 +0100

    working on omicsconnect demo on v3 model

---
+++ diagram.sh
@@ -0,0 +1,2 @@
+dot -Tpng -O "/Users/jvelde/github/molgenis_apps/WebContent/generated-doc/objectmodel-uml-diagram.dot"
+open /Users/jvelde/github/molgenis_apps/WebContent/generated-doc/objectmodel-uml-diagram.dot.png
