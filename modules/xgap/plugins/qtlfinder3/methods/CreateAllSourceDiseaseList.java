924eb387cebc70adf26c30f4b06b69f80f8c46a9
Mark-de-haan <markd3haan@hotmail.com>
2013-10-01 15:40:48 +0200

    New class to handle the All Human Sources selection

---
+++ modules/xgap/plugins/qtlfinder3/methods/CreateAllSourceDiseaseList.java
@@ -0,0 +1,39 @@
+package plugins.qtlfinder3.methods;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.molgenis.framework.server.MolgenisRequest;
+
+public class CreateAllSourceDiseaseList
+{
+	
+	public Map<String, List<String>> createAllSourceDiseaseList(MolgenisRequest request){
+
+		Map<String, List<String>> sourcesAndDiseases = new HashMap<String, List<String>>();
+		
+		String disease = new String();
+		String source = new String();
+		String[] split = null;
+		
+		for(String diseaseInput : request.getList("diseaseSelect")){
+
+			split = diseaseInput.toString().split("\\|\\|\\%\\|\\|");
+			
+			disease = split[0].toString();
+			source = split[1].toString();								
+
+			if(sourcesAndDiseases.keySet().contains(source)){
+				sourcesAndDiseases.get(source).add(disease);
+			}else{
+				List<String> diseaseList = new ArrayList<String>();
+				diseaseList.add(disease);
+				sourcesAndDiseases.put(source, diseaseList);
+			}	
+		}
+		
+		return sourcesAndDiseases;	
+	}
+}
